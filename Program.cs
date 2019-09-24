23c99fe80b328f26a19f3853193bae6db0eff207
Alibek <52044412+Alibek15@users.noreply.github.com>
2019-09-24 20:22:47 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car();
+            Car Car2 = new Car("Mersedes");
+            Car[] car3Cars = new Car[5];
+            for (int i = 0; i < car3Cars.Length; i++)
+            {
+                car3Cars[i] = new Car();
+            }
+
+            
+            Console.WriteLine();
+            Car1.MaxSpeed = 9;
+            Console.WriteLine(Car1.MaxSpeed);
+            Car1.Print(ref Car1);
+            Console.WriteLine();
+            Car1.Print();
+            Console.WriteLine();
+            Console.WriteLine(Car.carForeign);
+            Console.ReadKey();
+        }
+    }
+}
